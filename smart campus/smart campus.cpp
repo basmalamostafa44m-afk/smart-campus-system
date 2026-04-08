369b5f00ef901c885fee6b55d7db29e39f7837c6
basmalamostafa44m-afk <basmalamostafa44.m@gmail.com>
2026-04-08 20:37:13 +0200

    function add building

--- smart campus/smart campus.cpp
+++ smart campus/smart campus.cpp
@@ -7,7 +7,6 @@
 #include "admin.h"
 using namespace std;
 int main() {
-
 	const int Number_of_building = 10;
 	const int Number_of_Energy_reading = 30;
 	const int Number_of_Alerts = 20;
@@ -23,5 +22,4 @@ int main() {
 	int reading_counter = 3;// to keep track of the number of energy readings added
 	Alert alerts[Number_of_Alerts];// Array os alerts 
 	Admin admins[Number_of_Admins];// Array of admin 
-	
 }
