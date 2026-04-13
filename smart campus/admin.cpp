eb96fdf56cdba1fc771aa771aa15e44264f49622
sama essam <se560192@gmail.com>
2026-04-13 18:46:40 +0200

    Update admin.cpp

--- smart campus/admin.cpp
+++ smart campus/admin.cpp
@@ -6,15 +6,26 @@
 #include "admin.h"
 using namespace std;
 //define function to validate admin login ---> sama
+int current_admin_numbers = 0;
 bool Admin_login(string inputUser_name, string inputPassword) {
-	for (int i = 0; i < Number_of_Admins; i++) {
-		if (admins[i].user_name == inputUser_name && admins[i].password == inputPassword)
-			return true;
+		for (int i = 0; i < current_admin_numbers; i++) {
+			if (admins[i].user_name == inputUser_name && admins[i].password == inputPassword)
+				return true;
 	}
 	return false;
 }
-
-
+void admin_signup(string newuser, string newpass)
+{
+	if (current_admin_numbers < Number_of_Admins) {
+		admins[current_admin_numbers].Admin_ID = current_admin_numbers + 1;
+		admins[current_admin_numbers].user_name = newuser;
+		admins[current_admin_numbers].password = newpass;
+		current_admin_numbers++;
+		cout << " Admin created successfully" << endl;
+	}
+	else
+		cout << " sorry! Admin database is full ";
+}
 
 //define function to sign up new admin ---> sama
 
